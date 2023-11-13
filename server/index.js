ddb9e9aa3f50adaf0d7fdeeb438f870b611e9de1
ratasorin <ratasorin0@gmail.com>
2023-11-13 14:27:44 +0200

    [Fix] Listen to `arp` broadcasts as a way to demonstrate network multicasting

--- server/index.js
+++ server/index.js
@@ -1,75 +1,30 @@
-const express = require("express");
+const CURRENT_DEVICE_IP = "192.168.0.4";
+const cap = require("cap").Cap;
+const decoders = require("cap").decoders;
+const PROTOCOL = decoders.PROTOCOL;
+const FILTER = "arp";
 
-var Cap = require("cap").Cap;
-var decoders = require("cap").decoders;
-var PROTOCOL = decoders.PROTOCOL;
+const c = new cap();
+const device = cap.findDevice(CURRENT_DEVICE_IP);
+const bufSize = 10 * 1024 * 1024;
+const buffer = Buffer.alloc(65535);
 
-var c = new Cap();
-var device = Cap.findDevice("192.168.0.4");
-var filter = "tcp and dst port 80";
-var bufSize = 10 * 1024 * 1024;
-var buffer = Buffer.alloc(65535);
-
-var linkType = c.open(device, filter, bufSize, buffer);
+const linkType = c.open(device, FILTER, bufSize, buffer);
 
 c.setMinBytes && c.setMinBytes(0);
 
-c.on("packet", function (nbytes, trunc) {
+c.on("packet", function (bytes, trunc) {
   console.log(
-    "packet: length " + nbytes + " bytes, truncated? " + (trunc ? "yes" : "no")
+    "packet: length " + bytes + " bytes, truncated? " + (trunc ? "yes" : "no")
   );
 
-  // raw packet data === buffer.slice(0, nbytes)
-
   if (linkType === "ETHERNET") {
-    var ret = decoders.Ethernet(buffer);
+    const ret = decoders.Ethernet(buffer);
 
-    if (ret.info.type === PROTOCOL.ETHERNET.IPV4) {
-      console.log("Decoding IPv4 ...");
-
-      ret = decoders.IPV4(buffer, ret.offset);
-      console.log("from: " + ret.info.srcaddr + " to " + ret.info.dstaddr);
-
-      if (ret.info.protocol === PROTOCOL.IP.TCP) {
-        var datalen = ret.info.totallen - ret.hdrlen;
-
-        console.log("Decoding TCP ...");
-
-        ret = decoders.TCP(buffer, ret.offset);
-        console.log(
-          " from port: " + ret.info.srcport + " to port: " + ret.info.dstport
-        );
-        datalen -= ret.hdrlen;
-        console.log(
-          buffer.toString("binary", ret.offset, ret.offset + datalen)
-        );
-      } else if (ret.info.protocol === PROTOCOL.IP.UDP) {
-        console.log("Decoding UDP ...");
-
-        ret = decoders.UDP(buffer, ret.offset);
-        console.log(
-          " from port: " + ret.info.srcport + " to port: " + ret.info.dstport
-        );
-        console.log(
-          buffer.toString("binary", ret.offset, ret.offset + ret.info.length)
-        );
-      } else
-        console.log(
-          "Unsupported IPv4 protocol: " + PROTOCOL.IP[ret.info.protocol]
-        );
-    } else
+    if (ret.info.type === PROTOCOL.ETHERNET.ARP) {
+      console.log("Decoding ARP ...");
+    } else {
       console.log("Unsupported Ethertype: " + PROTOCOL.ETHERNET[ret.info.type]);
+    }
   }
-});
-
-const app = express();
-const port = 80;
-
-app.get("/hello-world", (req, res) => {
-  console.log("should see!");
-  res.send("Hello World!");
-});
-
-app.listen(port, "192.168.0.4", () => {
-  console.log(`Example app listening on port ${port}`);
 });
